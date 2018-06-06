2552175593ec45f552eba6e2ee3e3da9b5d29a60
Wehbiels <Lara.Wehbie@gmail.com>
2018-06-06 15:06:31 -0400

    js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import kitten from './kitten.json'
+import Wrapper from './components/Wrapper'
+import Navpills from './components/Navpills'
+import Title from './components/Title'
+import CatCard from './components/CatCard'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        kitten: kitten,
+        unselectedKitten: kitten
+    }
+
+    componentDidMount() {
+    }
+
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selectDog = breed => {
+        const findKitten = this.state.unselectedKitten.find(item => item.breed === breed);
+
+        if(findDog === undefined) {
+            // failure to select a new cat
+            this.setState({ 
+                message: "You guessed incorrectly! TRY AGAIN",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                kitten: kitten,
+                unselectedKitten: Kitten
+            });
+        }
+        else {
+            // success to select a new cat
+            const newKitten = this.state.unselectedKitten.filter(item => item.breed !== breed);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                kitten: kitten,
+                unselectedKitten: newKitten
+            });
+        }
+
+        this.shuffleArray(dogs);
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Navpills
+                    message={this.state.message}
+                    curScore={this.state.curScore}
+                    topScore={this.state.topScore}
+                />
+                <Title />
+                {
+                    this.state.kitten.map(dog => (
+                        <CatCard
+                            breed={kitten.breed}
+                            image={kitten.image}
+                            selectKitten={this.selectKitten} 
+                            curScore={this.state.curScore}
+                        />
+                    ))
+                }
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
